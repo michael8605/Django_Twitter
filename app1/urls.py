ff28a28db3eec0c7bcb93e1363721ac433c54266
michael8605 <64383152+michael8605@users.noreply.github.com>
2021-08-11 16:02:33 -0400

    changes

--- app1/urls.py
+++ app1/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.home, name = 'home'),
-    path('post_twitter', views.post_twitter, name = 'post_twitter'),
+    path('home/',views.home, name = 'home'),
+    path('', views.post_twitter, name = 'post_twitter'),
     path('reply/<int:id>/', views.reply, name = 'reply'),
     path('tweet_detail/<int:id>', views.tweet_detail, name = 'tweet_detail'),
     path('process_form', views.process_form, name = 'process_form'),
