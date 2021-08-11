916191e9d1ec5d41c798165095397864ea31cc17
michael8605 <64383152+michael8605@users.noreply.github.com>
2021-08-11 15:54:48 -0400

    Changed Homepage URL

--- app1/urls.py
+++ app1/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/',views.home, name = 'home'),
+    path('',views.home, name = 'home'),
     path('post_twitter', views.post_twitter, name = 'post_twitter'),
     path('reply/<int:id>/', views.reply, name = 'reply'),
     path('tweet_detail/<int:id>', views.tweet_detail, name = 'tweet_detail'),
